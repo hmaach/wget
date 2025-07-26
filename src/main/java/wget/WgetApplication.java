95b9e02efd948c56738f648ef7c616cee2acd1a6
Hamza Maach <hamzamaach56@gmail.com>
2025-07-26 15:39:12 +0100

    Implemented -i <file> to load URLs from a file

--- src/main/java/wget/WgetApplication.java
+++ src/main/java/wget/WgetApplication.java
@@ -1,34 +1,43 @@
 package wget;
 
+import java.util.List;
+
+import org.apache.commons.cli.ParseException;
+
+import wget.cli.ArgumentParser;
 import wget.download.Downloader;
-// import org.apache.commons.cli.ParseException;
+import wget.utils.FileUtils;
 
 public class WgetApplication {
 
     public void run(String[] args) {
-        // ArgumentParser parser;
-        // try {
-        //     parser = new ArgumentParser(args);
-        // } catch (ParseException e) {
-        //     System.out.println("Error parsing arguments: " + e.getMessage());
-        //     return;
-        // }
+        ArgumentParser parser;
+        try {
+            parser = new ArgumentParser(args);
+        } catch (ParseException e) {
+            System.out.println("Error parsing arguments: " + e.getMessage());
+            return;
+        }
 
-        // // No URL or input file? Show help.
-        // if (!parser.hasOption("i") && parser.getUrls().length == 0) {
-        //     parser.printHelp();
-        //     return;
-        // }
+        // No URL or input file? Show help.
+        if (!parser.hasOption("i") && parser.getUrls().length == 0) {
+            parser.printHelp();
+            return;
+        }
         Downloader downloader = new Downloader();
 
-        // if (parser.hasOption("i")) {
-        //     // TODO: read URLs from input file
-        //     System.out.println("Input file parsing not implemented yet.");
-        // }
-        // for (String url : parser.getUrls()) {
-        //     downloader.downloadFile(url);
-        // }
-        for (String url : args) {
+        if (parser.hasOption("i")) {
+            // TODO: I need to download asynchronously
+            List<String> urls;
+            try {
+                urls = FileUtils.readFile(parser.getOptionValue("i"));
+                parser.setUrls(urls.toArray(new String[0])); // Creates an empty array to let Java know the target type
+            } catch (Exception e) {
+                System.err.printf("ERROR: reading file content: %s", e.getMessage());
+            }
+        }
+
+        for (String url : parser.getUrls()) {
             downloader.downloadFile(url);
         }
     }
--- src/main/java/wget/utils/FileUtils.java
+++ src/main/java/wget/utils/FileUtils.java
@@ -1,6 +1,23 @@
 package wget.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileUtils {
+
+    public static List<String> readFile(String path) throws IOException {
+        List<String> urls = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                urls.add(line.trim());
+            }
+        }
+        return urls;
+    }
 
     public static String extractFileName(String url) {
         return url.substring(url.lastIndexOf('/') + 1).split("\\?")[0];
